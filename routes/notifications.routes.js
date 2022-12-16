d45848e9424cd00f24b8fbb459a74f563390d291
DanielDerma <danielderma94@gmail.com>
2022-12-16 14:46:30 -0600

    sendPushNotification

--- routes/notifications.routes.js
+++ routes/notifications.routes.js
@@ -4,11 +4,12 @@ const router = Router();
 
 const admin = require("firebase-admin");
 const db = admin.firestore();
+const { Timestamp } = admin.firestore;
 
 let expo = new Expo();
 
 router.get("/sendPushNotification", async (req, res) => {
-  const route = "tecDelicias";
+  const { title, body, route } = req.body;
   const users = await db.collection("users").where("route", "==", route).get();
 
   const validIds = users.docs.map((user) => user.id);
@@ -21,23 +22,25 @@ router.get("/sendPushNotification", async (req, res) => {
 
   let messages = [];
   for (let pushToken of allTokens) {
-    // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
-
-    // Check that all your push tokens appear to be valid Expo push tokens
     if (!Expo.isExpoPushToken(pushToken)) {
       console.error(`Push token ${pushToken} is not a valid Expo push token`);
       continue;
     }
 
-    // Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
     messages.push({
-      to: pushToken,
+      title,
+      body,
       sound: "default",
-      body: "This is a test notification",
-      title: "hola",
+      to: pushToken,
+      createdAt: Timestamp.now(),
       data: { withSome: "data" },
     });
   }
+  const alumno = {
+    grupo: "a",
+    grado: 5,
+    calificaion: 6,
+  };
 
   let chunks = expo.chunkPushNotifications(messages);
   let tickets = [];
@@ -46,10 +49,6 @@ router.get("/sendPushNotification", async (req, res) => {
       let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
       console.log(ticketChunk);
       tickets.push(...ticketChunk);
-      // NOTE: If a ticket contains an error code in ticket.details.error, you
-      // must handle it appropriately. The error codes are listed in the Expo
-      // documentation:
-      // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
     } catch (error) {
       console.error(error);
     }
